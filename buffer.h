4bfcb39f4818559663f1360a1e59b24e7f604045
jlks96 <joshlee_1996@hotmail.com>
2018-04-07 16:59:35 +0800

    -buffer code

---
+++ buffer.h
@@ -0,0 +1,27 @@
+#ifndef __Q_HEADER__
+#define __Q_HEADER__
+
+#include <pthread.h>
+#include <semaphore.h>
+
+//# of entries in the queue
+#define QLEN		10
+
+// Length of each queue entry in bytes
+#define ENTRY_SIZE	64
+
+typedef struct
+{
+	char data[QLEN][ENTRY_SIZE];
+	int len[QLEN];
+	int front, back;
+	int count;
+	sem_t empty, full;
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+} TBuffer;
+
+void initBuffer(TBuffer *buffer);
+void enq(TBuffer *buffer, const char *data, int len);
+int deq(TBuffer *buffer, char *data);
+#endif
+
